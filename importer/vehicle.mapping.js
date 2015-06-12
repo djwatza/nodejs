4574aa37ce1235839e3e680d3bfb9b7a8d72f7ed
autodealio <rhinocerous+autodealio@gmail.com>
2015-06-11 23:16:27 -0700

    fix mapping and car css

--- importer/vehicle.mapping.js
+++ importer/vehicle.mapping.js
@@ -1,144 +1,145 @@
-{
-        "mappings": {
-            "zip_code": {
-                "properties": {
-                    "city": {
-                        "type": "string"
+    {
+            "mappings": {
+                "zip_code": {
+                    "properties": {
+                        "city": {
+                            "type": "string"
+                        },
+                        "code": {
+                            "type": "string"
+                        },
+                        "country": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "latitude": {
+                            "type": "float"
+                        },
+                        "longitude": {
+                            "type": "float"
+                        },
+                        "pin" : {
+                            "properties" : {
+                                "location" : {
+                                    "type" : "geo_point"
+                                }
+                            }
+                        },
+                        "region": {
+                            "type": "string"
+                        },
+                        "state": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "state_name": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "zip_code": {
+                            "type": "integer",
+                            "index" : "not_analyzed"
+                        }
+                    }
+
+                },
+                "vehicle": {
+                    "_parent" : {
+                        "type" : "zip_code"
                     },
-                    "code": {
-                        "type": "string"
-                    },
-                    "country": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "latitude": {
-                        "type": "float"
-                    },
-                    "longitude": {
-                        "type": "float"
-                    },
-                    "pin" : {
-                        "properties" : {
-                            "location" : {
-                                "type" : "geo_point"
+                    "properties": {
+                        "certified_preowned": {
+                            "type": "boolean"
+                        },
+                        "city": {
+                            "type": "string",
+                            "index": "not_analyzed"
+                        },
+                        "comments": {
+                            "type": "string"
+                        },
+                        "dealer_id": {
+                            "type": "integer",
+                            "index" : "not_analyzed"
+                        },
+                        "exterior_color": {
+                            "type": "string"
+                        },
+                        "features": {
+                            "type": "string"
+                        },
+                        "image_urls": {
+                            "type": "string"
+                        },
+                        "interior_color": {
+                            "type": "string"
+                        },
+                        "make": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "mileage": {
+                            "type": "integer"
+                        },
+                        "model": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "num_cylinders": {
+                            "type": "integer"
+                        },
+                        "num_doors": {
+                            "type": "integer"
+                        },
+                        "price": {
+                            "type": "float"
+                        },
+                        "program_id": {
+                            "type": "integer",
+                            "index" : "not_analyzed"
+                        },
+                        "radius": {
+                            "type": "string"
+                        },
+                        "series": {
+                            "type": "string"
+                        },
+                        "state": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "stock_number": {
+                            "type": "string"
+                        },
+                        "today": {
+                            "type": "date"
+                        },
+                        "transmission_type": {
+                            "type": "string"
+                        },
+                        "vehicle_id": {
+                            "type": "string",
+                            "index" : "not_analyzed"
+                        },
+                        "vin": {
+                            "type": "string"
+                        },
+                        "year": {
+                            "type": "integer",
+                            "index" : "not_analyzed"
+                        },
+                        "zip_code": {
+                            "type": "integer",
+                            "index" : "not_analyzed"
+                        },
+                        "pin" : {
+                            "properties" : {
+                                "location" : {
+                                    "type" : "geo_point"
+                                }
                             }
                         }
-                    },
-                    "region": {
-                        "type": "string"
-                    },
-                    "state": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "state_name": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "zip_code": {
-                        "type": "integer",
-                        "index" : "not_analyzed"
                     }
                 }
-
-            },
-            "vehicle": {
-                "_parent" : {
-                    "type" : "zip_code"
-                },
-                "properties": {
-                    "certified_preowned": {
-                        "type": "boolean"
-                    },
-                    "city": {
-                        "type": "string"
-                    },
-                    "comments": {
-                        "type": "string"
-                    },
-                    "dealer_id": {
-                        "type": "integer",
-                        "index" : "not_analyzed"
-                    },
-                    "exterior_color": {
-                        "type": "string"
-                    },
-                    "features": {
-                        "type": "string"
-                    },
-                    "image_urls": {
-                        "type": "string"
-                    },
-                    "interior_color": {
-                        "type": "string"
-                    },
-                    "make": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "mileage": {
-                        "type": "integer"
-                    },
-                    "model": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "num_cylinders": {
-                        "type": "integer"
-                    },
-                    "num_doors": {
-                        "type": "integer"
-                    },
-                    "price": {
-                        "type": "float"
-                    },
-                    "program_id": {
-                        "type": "integer",
-                        "index" : "not_analyzed"
-                    },
-                    "radius": {
-                        "type": "string"
-                    },
-                    "series": {
-                        "type": "string"
-                    },
-                    "state": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "stock_number": {
-                        "type": "string"
-                    },
-                    "today": {
-                        "type": "date"
-                    },
-                    "transmission_type": {
-                        "type": "string"
-                    },
-                    "vehicle_id": {
-                        "type": "string",
-                        "index" : "not_analyzed"
-                    },
-                    "vin": {
-                        "type": "string"
-                    },
-                    "year": {
-                        "type": "integer",
-                        "index" : "not_analyzed"
-                    },
-                    "zip_code": {
-                        "type": "integer",
-                        "index" : "not_analyzed"
-                    },
-					"pin" : {
-						"properties" : {
-							"location" : {
-								"type" : "geo_point"
-							}
-						}
-					}                    
-                }
             }
-        }
-}
+    }
